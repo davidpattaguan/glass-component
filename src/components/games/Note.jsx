020f89f71c836005a40d1dfd9be612896ea63411
David Niño Angelo Pattaguan <wpattaguandna@gmail.com>
2022-01-24 11:41:06 +0800

    updated styles.css

--- src/components/games/Note.jsx
+++ src/components/games/Note.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 const Note = () => {
   return (
-    <li className="note-item">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, nulla. Velit
-      doloribus similique laborum saepe corporis minima ad odio
-    </li>
+    <li className="note-item">View on 1920x1080 reso. Not yet responsive</li>
   );
 };
 
